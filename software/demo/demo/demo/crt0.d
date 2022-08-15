1a63e7e4b1744add2366991f2103c702a3c55dd9
Lefteris D. Batzolis <e.batzolis@gmail.com>
2022-08-15 10:50:38 +0300

    removed folder dedup/impoved programmer script

--- software/demo/demo/demo/crt0.d
+++
@@ -1,1 +0,0 @@
-crt0.o: /home/iamme/litex/litex/soc/cores/cpu/vexriscv/crt0.S
--- software/programmer.py
+++ software/programmer.py
@@ -1,3 +1,11 @@
+#egbatzo stream programmer using litex\migen 
+
+
+#just add the bistream with name bitstream.sof 
+#inside the current folder/connect the fpga/
+#and call this script
+
+
 from litex.build.altera.programmer import USBBlaster
 
 prog = USBBlaster()
